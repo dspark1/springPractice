652f7340e0d15486ed2014d985cac0d722e72459
jakar <jakar@DESKTOP-1O4RVK1>
2022-02-24 01:57:01 +0900

    Commit - DAY02_2.AOP 용어 및 기본 설정

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -13,42 +13,32 @@ public class BoardServiceImpl implements BoardService {
 
 	@Autowired
 	private BoardDAO boardDAO;
-	private LogAdvice logAdvice;
-	
-	public BoardServiceImpl() {
-		logAdvice = new LogAdvice();
-	}
 	
 	@Override
 	public void insertBoard(BoardVO vo) {
-		logAdvice.printLog();
 		boardDAO.insertBoard(vo);
 		
 	}
 
 	@Override
 	public void updateBoard(BoardVO vo) {
-		logAdvice.printLog();
 		boardDAO.updateBoard(vo);
 		
 	}
 
 	@Override
 	public void deleteBoard(BoardVO vo) {
-		logAdvice.printLog();
 		boardDAO.deleteBoard(vo);
 		
 	}
 
 	@Override
 	public BoardVO getBoard(BoardVO vo) {
-		logAdvice.printLog();
 		return boardDAO.getBoard(vo);
 	}
 
 	@Override
 	public List<BoardVO> getBoardList(BoardVO vo) {
-		logAdvice.printLog();
 		return boardDAO.getBoardList(vo);
 	}
 }
