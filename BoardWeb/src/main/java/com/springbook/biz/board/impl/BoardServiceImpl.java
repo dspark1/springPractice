36125cf14a62a43f2fc81d9d6d30ec466d1ca09d
jakar <jakar@DESKTOP-1O4RVK1>
2022-02-24 01:19:47 +0900

    Commit - DAY02_1.1.AOP 이해하기

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -6,39 +6,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.common.LogAdvice;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
 
 	@Autowired
 	private BoardDAO boardDAO;
+	private LogAdvice logAdvice;
+	
+	public BoardServiceImpl() {
+		logAdvice = new LogAdvice();
+	}
 	
 	@Override
 	public void insertBoard(BoardVO vo) {
+		logAdvice.printLog();
 		boardDAO.insertBoard(vo);
 		
 	}
 
 	@Override
 	public void updateBoard(BoardVO vo) {
+		logAdvice.printLog();
 		boardDAO.updateBoard(vo);
 		
 	}
 
 	@Override
 	public void deleteBoard(BoardVO vo) {
+		logAdvice.printLog();
 		boardDAO.deleteBoard(vo);
 		
 	}
 
 	@Override
 	public BoardVO getBoard(BoardVO vo) {
+		logAdvice.printLog();
 		return boardDAO.getBoard(vo);
 	}
 
 	@Override
 	public List<BoardVO> getBoardList(BoardVO vo) {
+		logAdvice.printLog();
 		return boardDAO.getBoardList(vo);
 	}
-
 }
