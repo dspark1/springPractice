52e56b9b743c50c69769f20634a811c1c4da7db5
deuksu-park <jakarta153@naver.com>
2022-02-26 21:16:42 +0900

    conflict resolve

--- BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,7 +1,10 @@
 package com.springbook.biz.common;
 
 public class LogAdvice {
+<<<<<<< HEAD
 	
+=======
+>>>>>>> aeb7f40b32ad1a2ac32a827a3c80864c0c69206a
 	public void printLog() {
 		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
 	}
