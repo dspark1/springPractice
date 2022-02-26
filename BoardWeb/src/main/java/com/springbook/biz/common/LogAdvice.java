295215069327ff4d4774be16332364405e91d636
deuksu-park <jakarta153@naver.com>
2022-02-26 21:19:45 +0900

    conflict resolve

--- BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -2,7 +2,10 @@ package com.springbook.biz.common;
 
 public class LogAdvice {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	
+=======
+>>>>>>> aeb7f40b32ad1a2ac32a827a3c80864c0c69206a
 =======
 >>>>>>> aeb7f40b32ad1a2ac32a827a3c80864c0c69206a
 	public void printLog() {
